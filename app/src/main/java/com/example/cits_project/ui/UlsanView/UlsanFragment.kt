1e2f16219b67a44d5df3e892f548012f70f62b98
yeller <103626708+yeobeomhwi@users.noreply.github.com>
2023-11-23 12:24:24 +0900

    [11.2] 울산 위도경도 데이터 적용

--- app/src/main/java/com/example/cits_project/ui/UlsanView/UlsanFragment.kt
+++ app/src/main/java/com/example/cits_project/ui/UlsanView/UlsanFragment.kt
@@ -10,7 +10,6 @@ import com.example.cits_project.Ulsan.LocationService.Location_to_Firebase
 import com.example.cits_project.databinding.FragmentSlideshowBinding
 import com.google.firebase.database.DatabaseReference
 
-<<<<<<< HEAD
 class UlsanFragment : Fragment() {
     // ID_to_Firebase 인스턴스 생성
     private val idToFirebase = ID_to_Firebase()
@@ -19,13 +18,6 @@ class UlsanFragment : Fragment() {
     private val locationToFirebase = Location_to_Firebase()
 
     // View Binding을 사용하기 위한 Binding 변수
-=======
-public class UlsanFragment : Fragment() {
-    //private val serviceKey = "Rp5a%2Bo7IpQ8GkuHupL0lqV6PSZmT3PaVE2Psd0tID1lSXjrroHKrnzpHnf%2B%2Fw4%2BLU9I8XubDag6kYCG6hu2rPA%3D%3D"
-    private val serviceKey = "TOlfl5zsDX0idc1uqdtoVkQkk7oSlUV+Mqks/OYbEuYjRtgy8j+4Vv4rrFOFQm9YHCIOlPr91KwSNqe0yJrSEg=="
-    //private val serviceKey = "SSzwenl0pt9fP0H0Lzv+JTXtRoFXfzc9BHGAnOe9sRgnFUZ4wjmzRhrnOK1yEY0hNrThwhq3RpdIDzvH2h6Lgw=="
-
->>>>>>> 04966509537f212a7a5ad54da74320ac23724a7d
     private var _binding: FragmentSlideshowBinding? = null
     private val binding get() = _binding!!
 
@@ -47,70 +39,12 @@ public class UlsanFragment : Fragment() {
         // ID_to_Firebase의 fetchData 메서드 호출
         //idToFirebase.fetchData()
 
-<<<<<<< HEAD
         // Location_to_Firebase fetchData 메서드 호출
         locationToFirebase.fetchData()
 
         return binding.root
     }
 
-=======
-        val version = "*"
-
-        CITSIdRepository.getCITSIdService(serviceKey, version,
-            onSuccess = { citsIdResponse ->
-                // 성공적으로 응답을 받았을 때 실행되는 부분
-                val filteredItems = citsIdResponse?.body?.items?.filter { it.offerType == "SIG" }
-
-                val filteredItemsCount = filteredItems?.size
-                val linkIds = filteredItems?.map { it.linkId }?.joinToString(", ")
-                Log.d("CITSResponse", "Success: filteredItemsCount = $filteredItemsCount")
-                Log.d("CITSResponse", "Success: linkIds = $linkIds")
-
-                linkIds?.split(", ")?.chunked(10)?.forEach { chunk ->
-                    // Process each chunk of 10 linkIds
-                    chunk.forEach { individualLinkId ->
-                        // Convert the entire string to a numeric value
-                        val numericLinkId = individualLinkId.filter { it.isDigit() }.toLongOrNull()
-
-                        // Check if conversion was successful before using the value
-                        if (numericLinkId != null) {
-                            getCITSLocationService(numericLinkId.toString())
-                            //Log.d("linkId_num", "individualLinkId: $numericLinkId")
-                        } else {
-                           // Log.e("linkId_num", "Conversion to numeric value failed: $individualLinkId")
-                        }
-                    }
-                }
-            }
-        ) { error ->
-            // 오류가 발생했을 때 실행되는 부분
-            Log.e("CITSResponse", "Error: $error")
-        }
-
-
-
-        return root
-    }
-
-    private fun getCITSLocationService(linkId: String) {
-        // linkId와 type을 활용하여 getCITSLocationService 호출
-        locationRepository.getCITSLocationService(serviceKey, linkId,
-            onSuccess = { citsLocationResponse ->
-                if(citsLocationResponse?.body != null){
-                    val Itemss = citsLocationResponse?.body
-                    Log.d("loc","fsdf= $Itemss")
-                }
-
-            }
-        ) { error ->
-            // 오류가 발생했을 때 실행되는 부분
-            Log.e("CITSLocationResponse", "Error: $error")
-        }
-    }
-
-
->>>>>>> 04966509537f212a7a5ad54da74320ac23724a7d
     override fun onDestroyView() {
         super.onDestroyView()
         // View Binding 해제
